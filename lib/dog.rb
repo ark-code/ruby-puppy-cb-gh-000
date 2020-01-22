f3423aa9012e006b01b4888b496f200825bb9673
Akshay K <akshaykunti1@gmail.com>
2020-01-22 06:30:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,20 @@
 class Dog 
   @@all = []
   def initialize
+    save 
+  end
+  
+  def all 
+    @all 
+  end 
+  
+  def print_all 
+    @all.each do |e|
+      puts e 
+    end 
+  end
+
+  def save 
     @all << self  
   end
 
